46a76629d96d888f1dde7cea0db26383965962fa
Patrick <patrickdiehl@lsu.edu>
2023-06-20 16:34:27 -0500

    Add license header

--- coupling-approach-1.py
+++ coupling-approach-1.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
+#This file is part of https://github.com/diehlpk/reusommer21.
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 3.
+#
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+#
 # Coupling using the displacement for MDCM
 # @author patrickdiehl@lsu.edu
 # @author serge.prudhomme@polymtl.ca
@@ -11,9 +20,6 @@ import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.ticker import FormatStrFormatter
 from scipy import interpolate
-
-# pgf_with_latex = {"text.usetex": True, "font.size" : 12, "pgf.preamble" : [r'\usepackage{xfrac}'] }
-
 
 example = sys.argv[1]
 
